1837460715fdd53117d38cbd8a316cd188a69a0b
Tim Sheiner <tim.sheiner@meraki.net>
2026-02-10 15:30:55 -0800

    change app port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
@@ -6,7 +6,7 @@ export default defineConfig({
     strictPort: true,
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     sourcemap: true,
-  }
-})
+  },
+});
